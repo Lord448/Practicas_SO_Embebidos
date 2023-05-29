e688076c26eea8ac17430985744ce425e8232ddd
Coffee448 <95953394+Coffee448@users.noreply.github.com>
2023-05-29 22:14:02 +0000

    Add files via upload
    Practica 5 agregada

---
+++ C/sucesion_numerica.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE *archivo;
+
+    archivo = fopen("sucesion_numerica.csv", "w");
+
+    if(archivo == NULL){
+        printf("Error al abrir el archivo deseado");
+        return(-1);
+    }
+
+    double potenciaN, sucesion[100];
+    char cadena[100];
+    int n = 1;
+
+    for(int i = 0; i< 20 ; i++){
+        potenciaN = pow(-1*n, 3);
+        sucesion[i] = 1/potenciaN;
+        sprintf(cadena, "%f ", sucesion[i]);
+        fprintf(archivo, cadena);
+        n++;
+    } 
+    fprintf(archivo,"\n"); 
+    fclose(archivo);
+    
+    return 0;
+}
